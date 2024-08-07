fbcbdaebe2e76b48e519dae6dfeda6133ab2a639
Doğukan <subulut416@gmail.com>
2024-08-07 17:56:09 +0300

    First homeworks

--- src/odevler/bolum01/Q11.java
+++ src/odevler/bolum01/Q11.java
@@ -4,7 +4,7 @@ public class Q11 {
     public static void main(String[] args) {
         int population = 312032486;
         double year_with_seconds = 365.0*24*60*60;
-        double birth = (double) year_with_seconds / 7;
+        double birth = year_with_seconds / 7;
         double death = year_with_seconds / 13;
         double immigrant = year_with_seconds / 45;
         System.out.println("In one year later population will increase "+ (int)(birth +immigrant - death) + " and new population will be " + (int)(population + (birth +immigrant - death)));
