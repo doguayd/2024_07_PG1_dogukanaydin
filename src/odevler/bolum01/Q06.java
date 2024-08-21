6ccd2bef8e1be40da0db5ace5590a65e2b82c60f
doguayd <69399650+doguayd@users.noreply.github.com>
2024-08-21 17:59:28 +0300

    Add files via upload

--- src/odevler/bolum01/Q06.java
+++ src/odevler/bolum01/Q06.java
@@ -2,8 +2,8 @@ package odevler.bolum01;
 
 public class Q06 {
     public static void main(String[] args) {
-        int Number = 9;
-        int Summation = (Number * (Number + 1)) / 2;
-        System.out.println(Summation);
+        int number = 9;
+        int summation = (number * (number + 1)) / 2;
+        System.out.println(summation);
     }
 }
--- src/odevler/bolum01/Q10.java
+++ src/odevler/bolum01/Q10.java
@@ -3,8 +3,8 @@ package odevler.bolum01;
 public class Q10 {
     public static void main(String[] args) {
         double km = 14.0, mile = km / 1.6;
-        double time = (45 * 60) + 30.0 , total_hour = time / 3600;
-        double average_speed_in_miles = mile / total_hour;
-        System.out.println("If a runner runs 14 kilometers in 45 minutes and 30 seconds, runner's average speed in miles is " + average_speed_in_miles);
+        double time = (45 * 60) + 30.0 , totalHour = time / 3600;
+        double averageSpeedInMiles = mile / totalHour;
+        System.out.println("If a runner runs 14 kilometers in 45 minutes and 30 seconds, runner's average speed in miles is " + averageSpeedInMiles);
     }
 }
