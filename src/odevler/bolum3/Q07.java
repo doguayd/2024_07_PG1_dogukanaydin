221f507cfbf92d29216cb8e60ab98e8815d691b8
Doğukan <subulut416@gmail.com>
2024-09-10 22:16:46 +0300

    devam

---
+++ src/odevler/bolum3/Q07.java
@@ -0,0 +1,69 @@
+package odevler.bolum3;
+
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter an amount in double, for example 11.56: ");
+        double amount = input.nextDouble();
+
+        int remainingAmount = (int) (amount * 100);
+
+        int numberOfOneDollars = remainingAmount / 100;
+        remainingAmount = remainingAmount % 100;
+
+        int numberOfQuarters = remainingAmount / 25;
+        remainingAmount = remainingAmount % 25;
+
+        int numberOfDimes = remainingAmount / 10;
+        remainingAmount = remainingAmount % 10;
+
+        int numberOfNickels = remainingAmount / 5;
+        remainingAmount = remainingAmount % 5;
+
+        int numberOfPennies = remainingAmount;
+
+        System.out.println("Your amount " + amount + " consists of");
+
+        if (numberOfOneDollars == 1) {
+            System.out.println(" 1 dollar");
+        } else if (numberOfOneDollars == 0) {
+            
+        } else {
+            System.out.println(" " + numberOfOneDollars + " dollars");
+        }
+
+        if (numberOfQuarters == 1) {
+            System.out.println(" 1 quarter");
+        } else if (numberOfQuarters == 0) {
+            
+        } else {
+            System.out.println(" " + numberOfQuarters + " quarters ");
+        }
+        if (numberOfDimes == 1) {
+            System.out.println(" 1 dime");
+        } else if (numberOfDimes == 0) {
+            
+        } else {
+            System.out.println(" " + numberOfDimes + " dimes");
+        }
+        if (numberOfNickels == 1) {
+            System.out.println(" 1 nickel");
+        } else if (numberOfNickels == 0) {
+
+        } else {
+            System.out.println(" " + numberOfNickels + " nickels");
+        }
+        if (numberOfPennies == 1) {
+            System.out.println(" 1 pennie");
+        } else if (numberOfPennies == 0) {
+
+        } else {
+            System.out.println(" " + numberOfPennies + " pennies");
+        }
+
+    }
+}
+
---
+++ src/odevler/bolum3/Q10.java
@@ -0,0 +1,11 @@
+package odevler.bolum3;
+
+import java.util.Scanner;
+
+public class Q10 {
+    public static void main(String[] args) {
+        Scanner input =new Scanner(System.in);
+
+
+    }
+}
